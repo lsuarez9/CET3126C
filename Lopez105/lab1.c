160568f9dbf6c116ed896cbcd589a1f3f707f36f
Luis M. Lopez <luis.lopez105@mymdc.net>
2025-09-29 21:28:03 -0400

    Add files via upload

---
+++ Lopez105/lab1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+#include <float.h>
+
+int main(){
+    // Exercise 1
+    printf("Lab #1!\n");
+
+    // Size of Integer
+    int max_int = INT_MAX;
+    int min_int = INT_MIN;
+
+    printf("Maximum value of int: %d\n", max_int);
+    printf("Minimum value of int: %d\n", min_int);
+    
+    // Size of Char
+    char max_char = CHAR_MAX;
+    char min_char = CHAR_MIN;
+
+    printf("Maximum value of char: %d\n", max_char);
+    printf("Minimum value of char: %d\n", min_char);
+
+    //Size of float
+    float max_float = FLT_MAX;
+    float min_float = FLT_MIN;
+
+    printf("Maximum value of float: %.10e\n", max_float);
+    printf("Minimum value of char: %.10e\n", min_float);
+  
+    //Exercise 2
+    // Define two uint8_t variables with values close to the maximum
+    uint8_t a = 200;
+    uint8_t b = 100;
+
+    // Add the two variables
+    uint8_t result = a + b;
+
+    // Print the values of a, b, and the result
+    printf("a = %u\n", a);
+    printf("b = %u\n", b);
+    printf("result = a + b = %u\n", result);
+
+    // The correct result without overflow
+    unsigned int correctResult = a + b;
+    printf("Correct result without overflow: %u\n", correctResult);
+    return 0;
+}
